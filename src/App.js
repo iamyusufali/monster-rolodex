5cbd97e6598bb6f0231d6d5fe8dd9f367294dc24
Yusuf Ali <yali98319@gmail.com>
2020-05-03 11:57:26 +0530

    update styles

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends React.Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value}) 
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filterdMonsters = monsters.filter(monster => {
@@ -29,9 +33,10 @@ class App extends React.Component {
     
     return (
       <div className='App'> 
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='Search here'
-          handleChange={(e) => this.setState({searchField: e.target.value}) }
+          handleChange={this.handleChange}
         />
         <CardList monsters={filterdMonsters} />
       </div>
